98f09f22ee2e0f0b06743f71ba242ab5b5a209cd
yeaminrabbi <yeaminrabbi308@gmail.com>
2022-08-03 15:13:11 +0600

    Vue setup and Components

---
+++ New folder/modal-project/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  
+  <h1>{{ title }}</h1>
+  <input type="text" name="" id="" ref="name">
+  <button @click="handleClick">click me </button>
+  <CustomModal />
+ 
+</template>
+ 
+<script>
+import CustomModal from './components/CustomModal'
+
+export default {
+    name: "App",
+    components: { 
+      CustomModal 
+      
+      },
+    data() {
+        return {
+            title: "My first Vue project"
+        };
+    },
+    methods: {
+        handleClick() {
+            console.log(this.$refs.name);
+            console.log(this.$refs.name.value);
+            this.$refs.name.classList.add("active");
+            this.$refs.name.focus();
+            this.$refs.name.value = "";
+        }
+    },
+  
+}
+</script>
+
+<style >
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline block;
+  padding-bottom: 10px;
+}
+</style>
